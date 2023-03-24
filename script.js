a956730765a53dd8ef9fc578ed7e1de58f8ded16
unknown <j.telher@gmail.com>
2023-03-24 02:01:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var menuBtn = document.getElementById("menuBtn");
+var sidebar = document.getElementById("sidebar");
+var menu = document.getElementById("menu");
+
+sidebar.style.right = "-250px";
+
+menuBtn.onclick = function(){
+    if(sidebar.style.right == "-250px"){
+        sidebar.style.right = "0";
+        menu.src = "/assets/close.png";
+    }
+    else{
+        sidebar.style.right = "-250px";
+        menu.src = "/assets/menu.png"
+    }
+}
