650c1b9b19465b4e16481a358f0cf5b138089471
unknown <j.telher@gmail.com>
2023-03-24 02:37:23 +0100

    secood commit

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ sidebar.style.right = "-250px";
 menuBtn.onclick = function(){
     if(sidebar.style.right == "-250px"){
         sidebar.style.right = "0";
-        menu.src = "/assets/close.png";
+        menu.src = "./assets/close.png";
     }
     else{
         sidebar.style.right = "-250px";
-        menu.src = "/assets/menu.png"
+        menu.src = "./assets/menu.png"
     }
 }
